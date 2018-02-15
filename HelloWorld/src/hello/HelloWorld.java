508768f590b1d55921b0ebb4b14e170916675434
bory <bory@iit221.iit.uni-miskolc.hu>
2018-02-15 15:14:30 +0100

    Add Hello World project

---
+++ HelloWorld/src/hello/HelloWorld.java
@@ -0,0 +1,22 @@
+package hello;
+/**
+ * 
+ * @author bory
+ *
+ */
+public class HelloWorld {
+/**
+ * 
+ * @param args main_args
+ */
+	public static void main(String[] args) {
+		//komment
+		/*
+		 * több
+		 * soros
+		 */
+		System.out.println("Hello World!");
+
+	}
+
+}
